61ba692aa54d7e35b31445c4d9ca7fa916fd7bfd
bence.ilcsik <ilcsikbence1@gmail.com>
2021-01-26 14:39:47 +0100

    Add RoundBegin event

---
+++ DuckyCSGOIntegration/Events/RoundBeginEvent.cs
@@ -0,0 +1,15 @@
+﻿using DuckyCSGOIntegration.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DuckyCSGOIntegration
+{
+    public partial class Events
+    {
+        public static void RoundBegin(CSGSI.Events.RoundBeginEventArgs e)
+        {
+            var config = ConfigurationManager.Configuration;
+        }
+    }
+}
