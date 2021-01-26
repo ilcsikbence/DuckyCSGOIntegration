08971bc4ac5205f76c16f71187a781ddbf0141ed
bence.ilcsik <ilcsikbence1@gmail.com>
2021-01-26 14:39:58 +0100

    Add RoundEnd event

---
+++ DuckyCSGOIntegration/Events/RoundEndEvent.cs
@@ -0,0 +1,15 @@
+﻿using DuckyCSGOIntegration.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DuckyCSGOIntegration
+{
+    public partial class Events
+    {
+        public static void RoundEnd(CSGSI.Events.RoundEndEventArgs e)
+        {
+            var config = ConfigurationManager.Configuration;
+        }
+    }
+}
