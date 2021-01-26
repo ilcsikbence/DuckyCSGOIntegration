5e8fd1de7108df2991d986d4f5bc397d6656a121
bence.ilcsik <ilcsikbence1@gmail.com>
2021-01-26 14:37:54 +0100

    Add layout language enums

--- DuckyCSGOIntegration/Configuration/Layout/Enums/Language.cs
+++ DuckyCSGOIntegration/Configuration/Layout/Enums/Language.cs
@@ -6,5 +6,11 @@ namespace DuckyCSGOIntegration.Configuration.Layout.Enums
 {
     public enum Language
     {
+        US = 0,
+        UK = 1,
+        HU = 2,
+        ES = 3,
+        RU = 4,
+        DE = 5
     }
 }
