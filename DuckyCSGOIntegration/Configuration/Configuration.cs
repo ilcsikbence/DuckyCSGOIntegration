e6780aac46c4213fb2779467219e434e1ef718d3
bence.ilcsik <ilcsikbence1@gmail.com>
2021-01-26 14:38:19 +0100

    Add configuration model

---
+++ DuckyCSGOIntegration/Configuration/Configuration.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DuckyCSGOIntegration.Configuration
+{
+    public class Configuration
+    {
+        public string Uri { get; }
+        public int Port { get; }
+        public Configuration()
+        {
+            Uri = "http://localhost:3000/";
+            Port = 3000;
+        }
+    }
+}
